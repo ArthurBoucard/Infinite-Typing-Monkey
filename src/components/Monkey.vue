a99ef52263ec841c32092b2f962786638583e780
arthurboucard <arthur.boucard@hotmail.fr>
2023-07-07 12:29:13 +0200

    basic text generation

---
+++ src/components/Monkey.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class="monkey">
+      <!-- Monkey image or animation goes here -->
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    name: 'MonkeyComponent' // Updated component name
+  };
+  </script>
+  
+  <style scoped>
+  .monkey {
+    /* Monkey styles go here */
+  }
+  </style>
+  
---
+++ src/components/TextGenerator.vue
@@ -0,0 +1,41 @@
+<template>
+    <div class="text-generator">
+      <div class="text-area">
+        <p>{{ generatedText }}</p>
+      </div>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    name: 'TextGenerator',
+    data() {
+      return {
+        generatedText: ''
+      };
+    },
+    mounted() {
+      // Generate and update text at regular intervals
+      setInterval(() => {
+        this.generatedText += getRandomText();
+      }, 500);
+    }
+  };
+  
+  function getRandomText() {
+    // Generate and return random text (you can customize this function)
+    const text = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789 -.,;:!?()';
+    return Array.from({ length: 10 }, () => text[Math.floor(Math.random() * text.length)]).join('');
+  }
+  </script>
+  
+  <style scoped>
+  .text-generator {
+    /* Text generator styles go here */
+  }
+  
+  .text-area {
+    /* Text area styles go here */
+  }
+  </style>
+  
